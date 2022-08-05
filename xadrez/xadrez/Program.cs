3273c5283780229a9af4beac245a4ae08475fe31
Martins Passos <matheus.passos@externos-br.cencosud.com>
2022-08-05 15:57:34 -0300

    Criando a classe 'Posicao'

---
+++ xadrez/xadrez/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " + P);
+            Console.ReadLine();
+        }
+    }
+}
