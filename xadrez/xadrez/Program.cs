8946bd44c8d350cf9570237977199491a273cddb
Martins Passos <matheus.passos@externos-br.cencosud.com>
2022-08-09 15:02:51 -0300

    começando a implementar o tabuleiro

--- xadrez/xadrez/Program.cs
+++ xadrez/xadrez/Program.cs
@@ -3,15 +3,12 @@ using tabuleiro;
 
 namespace xadrez
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Posicao P;
+            Tabuleiro tab = new Tabuleiro (8, 8);
 
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posicao: " + P);
             Console.ReadLine();
         }
     }
