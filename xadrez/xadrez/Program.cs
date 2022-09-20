a78accd45135336dc92d38fd0a066263b772234a
Matheus Passos <matheus.martinspassos@yahoo.com>
2022-09-20 15:51:34 -0300

    Criado a classe posicaoTabuleiro

--- xadrez/xadrez/Program.cs
+++ xadrez/xadrez/Program.cs
@@ -8,22 +8,10 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, ));
-
-                Tela.imprimirTabuleiro(tab);
-
-                Console.ReadLine();
-            }
-            catch(TabuleiroExeption e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoTabuleiro pos = new PosicaoTabuleiro('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
+            Console.ReadLine();
         }
     }
 }
